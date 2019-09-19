3de857b17a06404ca725cbeed3326c60109596d8
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-18 21:15:39 -0300

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard'
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Route path="/dashboard" component={Dashboard} />
+        </BrowserRouter>
+    )
+}
