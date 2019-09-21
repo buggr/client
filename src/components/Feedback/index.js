025d0788006872068e3104c103bdd1a3c8cfdfa0
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-21 19:39:29 -0300

    Add real time feedback component

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Spin, Rate } from 'antd'
+import { Spin, Rate, Button } from 'antd'
 
 import './style.scss'
 
@@ -10,7 +10,11 @@ export default class Feedback extends Component {
             ready: false,
             team: "buggr",
             rate: 0,
+            feedback: '',
+            loading: false,
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
@@ -18,6 +22,12 @@ export default class Feedback extends Component {
     }
 
     handleRate = rate => this.setState({ rate })
+
+    handleChange = event => this.setState({ [event.target.name]: event.target.value })
+
+    handleSubmit(){
+        this.setState({ loading: true })
+    }
 
     render(){
         return(
@@ -27,7 +37,25 @@ export default class Feedback extends Component {
                     ?   <>
                             <h1 className="presentation-name">A equipe <strong>{this.state.team}</strong> está apresentando!</h1>
                             <h1 className="presentation-opnion">Dê sua opnião!</h1>
-                            <Rate value={this.state.rate} onChange={rate => this.handleRate(rate)}></Rate>
+                            <Rate 
+                                value={this.state.rate} 
+                                onChange={rate => this.handleRate(rate)}
+                                style={{margin: "0 auto"}}
+                            />
+                            <textarea 
+                                placeholder="Deseja dizer algo para a equipe?"
+                                onChange={event => this.handleChange(event)}
+                                value={this.state.feedback}
+                                name="feedback"
+                                type="text"
+                            />
+                            <Button 
+                                htmlType="submit" 
+                                loading={this.state.loading}
+                                onClick={this.handleSubmit}
+                            >
+                                Votar
+                            </Button>
                         </>
                     :   <div className="feedback-loading">
                             <h1>Aguardando alguma sala ser aberta...</h1>
