2109ce83943be173c1bb13f21eb1b4ace064b83e
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-21 19:28:09 -0300

    Add feedback page

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Spin } from 'antd'
+import { Spin, Rate } from 'antd'
 
 import './style.scss'
 
@@ -7,20 +7,28 @@ export default class Feedback extends Component {
     constructor(props){
         super(props)
         this.state = {
-            ready: false
+            ready: false,
+            team: "buggr",
+            rate: 0,
         }
     }
 
     componentDidMount(){
-
+        this.setState({ ready: true })
     }
+
+    handleRate = rate => this.setState({ rate })
 
     render(){
         return(
             <div className="feedback-container">
                 {
                     this.state.ready
-                    ?   <h1>EAE</h1>
+                    ?   <>
+                            <h1 className="presentation-name">A equipe <strong>{this.state.team}</strong> está apresentando!</h1>
+                            <h1 className="presentation-opnion">Dê sua opnião!</h1>
+                            <Rate value={this.state.rate} onChange={rate => this.handleRate(rate)}></Rate>
+                        </>
                     :   <div className="feedback-loading">
                             <h1>Aguardando alguma sala ser aberta...</h1>
                             <Spin 
