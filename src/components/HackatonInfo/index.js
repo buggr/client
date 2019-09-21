2b35ccad558e4da93d6a7f1c40b1c93e76eb09b4
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-09-21 15:40:50 -0300

    Add firebase folder to gitignore

---
+++ src/components/HackatonInfo/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './style.scss'
+
+export default class HackatonInfo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <h1>TESTE</h1>
+        )
+    }
+}
